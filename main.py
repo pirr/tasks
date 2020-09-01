b1fc51090057657a953a3e0728cbd35d056815f3
alekseysmaga <tattoo1mag@gmail.com>
2020-09-01 23:37:07 +0200

    - added aileron model - problem with bank and aileron

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 import asyncio
-import random
+import math
 
 
-TIME = 0.0
-MAX_TURN_IN_SEC_DEGREES = 3
-MAX_BANK_DEGREES = 30
-CURRENT_BANK_DEGREES = 0
-MAX_SPEED = 350 * 1000 / 3600  # not used yet
 # R_MIN = SPEED ** 2 / 9.81 / math.tan(math.radians(MAX_BANK_DEGREES))
+# T_MIN = 0.64 * R_MIN / 9.81 / math.tan(math.radians(MAX_BANK_DEGREES))
 # MAX_TIME_TURN = R_MIN / SPEED  # simplified
-MAX_AILERON_COEFF = 1  # for simplified -1...1
-CURRENT_AILERON_COEFF = 0  # -1...0 - left down, right up 0..1 - left up, right down, 0 - no
+# MAX_AILERON_COEFF = 1  # for simplified -1...1
+MAX_TURN_IN_SEC_DEGREES = 3
+MAX_BANK_IN_SEC_DEGREES = 10
 
-CURRENT_COURSE = 5
+AILERON_VALUE_DEGREES = 0
+
+AILERON_TIME_ON_ONE_DEGREES = 0.2
+
+MAX_BANK_DEGREES = 30  # -30...30
+MAX_AILERON_DEGREES = 30  # -30...30
+
+CURRENT_AILERON_DEGREES = 0  # simplify -30...0 - left down, right up 0..30 - left up, right down
+CURRENT_BANK_DEGREES = 27
+CURRENT_COURSE = 354.1999999999999
+SPEED = 350 * 1000 / 3600  # m/s
+
 AUTOPILOT_TASK = None
 AILERON_TASK = None
 
 
-# TODO prowl?
 async def autopilot_task(new_course: float):
     global CURRENT_COURSE
     global AUTOPILOT_TASK
@@ -33,7 +40,7 @@ async def autopilot_task(new_course: float):
     print(f'change current course from {CURRENT_COURSE} to {new_course}')
     while True:
         asyncio.ensure_future(set_aileron_degrees(new_course))
-        await asyncio.sleep(0)
+        await asyncio.sleep(0.01)
 
 
 def get_course_diff(new_course: float):
@@ -51,49 +58,71 @@ def get_course_diff(new_course: float):
 
 
 async def set_aileron_degrees(course: float):
-    global CURRENT_AILERON_COEFF
+    global AILERON_VALUE_DEGREES
     global AILERON_TASK
-    global CURRENT_COURSE
 
     if AILERON_TASK is not None and not AILERON_TASK.cancelled():
         AILERON_TASK.cancel()
     AILERON_TASK = asyncio.current_task()
 
-    if course == CURRENT_COURSE:
-        CURRENT_AILERON_COEFF = 0
+    if abs(CURRENT_BANK_DEGREES) > MAX_BANK_DEGREES:
+        m = 1 if CURRENT_AILERON_DEGREES > 0 else -1
+        AILERON_VALUE_DEGREES = MAX_AILERON_DEGREES * m
         return
 
-    cource_diff = get_course_diff(course)
-    turn_on = abs(cource_diff) / MAX_TURN_IN_SEC_DEGREES
-    m = 1 if cource_diff > 1 else -1
+    course_diff = get_course_diff(course)
+    turn_time = calculate_turn_time(
+        turning_angle=abs(course_diff),
+        speed=SPEED,
+        bank_degrees=CURRENT_BANK_DEGREES,
+    )
 
-    if turn_on >= 1:
-        CURRENT_AILERON_COEFF = MAX_AILERON_COEFF * m
-    elif turn_on:
-        CURRENT_AILERON_COEFF = MAX_AILERON_COEFF * turn_on * m  # problem with unit
+    if course_diff == 0:
+        AILERON_VALUE_DEGREES = 0
+        return
+
+    m = 1 if course_diff > 1 else -1
+
+    AILERON_VALUE_DEGREES = turn_time / MAX_TURN_IN_SEC_DEGREES * m
+    if abs(AILERON_VALUE_DEGREES) > MAX_AILERON_DEGREES:
+        AILERON_VALUE_DEGREES = MAX_AILERON_DEGREES * m
+
     await asyncio.sleep(0)
+
+
+async def model_aileron():
+    global CURRENT_AILERON_DEGREES
+    print('RUN MODEL AILERON')
+    while True:
+        aileron_diff = -(CURRENT_AILERON_DEGREES - AILERON_VALUE_DEGREES)
+        m = 1 if aileron_diff >= 0 else -1
+        for i in range(int(abs(aileron_diff))):
+            CURRENT_AILERON_DEGREES += m
+            await asyncio.sleep(AILERON_TIME_ON_ONE_DEGREES)
+        aileron_diff = (abs(aileron_diff) - abs(int(aileron_diff))) * m
+        CURRENT_AILERON_DEGREES += aileron_diff
+        await asyncio.sleep(abs(aileron_diff) / AILERON_TIME_ON_ONE_DEGREES)
 
 
 async def model_bank():
     global CURRENT_BANK_DEGREES
     print('RUN MODEL BANK')
     while True:
-        # TODO smoothness factor, Speed ...
-        new_bank_degrees = CURRENT_AILERON_COEFF * MAX_BANK_DEGREES
-        aileron_diff = new_bank_degrees - CURRENT_BANK_DEGREES
-        m = 1 if aileron_diff >= 0 else -1
-        for i in range(int(abs(new_bank_degrees))):
-            CURRENT_BANK_DEGREES += m
-            aileron_diff -= 1 * m
+        change_on = (CURRENT_AILERON_DEGREES / MAX_AILERON_DEGREES)
+        if change_on == 0:
             await asyncio.sleep(0.1)
-        CURRENT_BANK_DEGREES += aileron_diff
-        await asyncio.sleep(0.1 * abs(aileron_diff))
+            continue
+        m = 1 if change_on > 0 else -1
+        for i in range(abs(int(change_on))):
+            CURRENT_BANK_DEGREES += MAX_BANK_IN_SEC_DEGREES * m
+            await asyncio.sleep(1)
+        change_on = (abs(change_on) - abs(int(change_on))) * m
+        CURRENT_BANK_DEGREES += change_on
+        await asyncio.sleep(abs(change_on))
 
 
-async def model_cource():
+async def model_course():
     global CURRENT_COURSE
-    global CURRENT_BANK_DEGREES
-    global MAX_TURN_IN_SEC_DEGREES
     print('RUN MODEL COURSE')
     while True:
         k = CURRENT_BANK_DEGREES / MAX_BANK_DEGREES
@@ -102,28 +131,33 @@ async def model_cource():
             CURRENT_COURSE += 360
         elif CURRENT_COURSE > 359:
             CURRENT_COURSE -= 360
-        await asyncio.sleep(1)
+        await asyncio.sleep(abs(k))
+
+
+def calculate_turn_time(turning_angle: float, speed: float, bank_degrees: float):
+    return (turning_angle / 360) * 0.64 * (speed / (9.81 * math.tan(math.radians(bank_degrees))))
 
 
 async def print_data():
     while True:
         print(f'CURRENT COURSE: {CURRENT_COURSE:6.2f} | '
               f'CURRENT BANK: {CURRENT_BANK_DEGREES:6.2f} | '
-              f'CURRENT AILERON {CURRENT_AILERON_COEFF:4.2f}')
+              f'CURRENT AILERON: {CURRENT_AILERON_DEGREES:4.2f}')
         await asyncio.sleep(1.5)
 
 
 async def main():
     asyncio.ensure_future(model_bank())
-    asyncio.ensure_future(model_cource())
+    asyncio.ensure_future(model_course())
+    asyncio.ensure_future(model_aileron())
     asyncio.ensure_future(print_data())
     await asyncio.sleep(0)
-    for i in range(4):
-        new_course = round(random.uniform(0.0, 12.0), 2)
-        # new_course = 8.222512943948026
+    for i in range(1):
+        # new_course = round(random.uniform(0.0, 359), 2)
+        new_course = 350
         print(f'NEW TASK COURSE: {new_course}')
         asyncio.ensure_future(autopilot_task(new_course))
-        await asyncio.sleep(random.randint(0, 15))
+        # await asyncio.sleep(random.randint(0, 15))
 
 
 if __name__ == '__main__':
